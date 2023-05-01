e82f76d3d1913fba1df5c90ec567397006ae206e
Vitaliy Emeliyantsev <gambala.rus@gmail.com>
2023-05-01 10:55:14 +0200

    Initial commit

---
+++ lib/code_spectator/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module CodeSpectator
+  VERSION = "0.1.0"
+end
---
+++ spec/code_spectator_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe CodeSpectator do
+  it "has a version number" do
+    expect(CodeSpectator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
